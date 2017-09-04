fa077912be6fca39acf2c43638baf38ad62e38ae
kusumanjali koneru <kusumanjalikoneru@kusumanjalis-MacBook-Air.local>
2017-09-04 17:02:16 -0500

    updated node app

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, HOST);
-console.log(`Server is running on http://${HOST}:${PORT}`);
+console.log(`Hello!! Server is running on http://${HOST}:${PORT}`);
